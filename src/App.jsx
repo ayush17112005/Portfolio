956ef089d029f2a1993ba073b05229cccadaac7b
ayush17112005 <saxenaayushman99@gmail.com>
2025-11-28 23:44:52 +0530

    Initial setup: Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <div className="text-3xl border-2 border-black bg-amber-600">
+        Tailwind is working!
+      </div>
+    </>
+  );
+}
+
+export default App;
